817a233581be436df3a88058b321ac7b0eb1238c
Dmitry <kolosovdv@rambler.ru>
2021-12-08 21:39:47 +0300

    2

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,6 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
+    #дописал
+    path('api/', include('api.urls')),
 ]
