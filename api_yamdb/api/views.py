cbe0d64d9cf8123b43948dbae77379b89228aa6d
DovganHub <asdovganyuk@gmail.com>
2021-12-08 22:05:06 +0300

    50%

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
-
+from rest_framework import filters, mixins, status, viewsets
+from reviews.models import Genre, Title
+from reviews.models import Category
+from reviews.serializers import CategorySerializer, GenreSerializer, TitleSerializer
 # Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()#.order_by('id')
+    serializer_class = CategorySerializer
+    # permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['=name']
+    lookup_field = 'slug'
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()#.order_by('id')
+    serializer_class = GenreSerializer
+    # permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['=name']
+    lookup_field = 'slug'
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()#.order_by('id')
+    serializer_class = TitleSerializer
+    # permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['=name']
+    lookup_field = 'slug'
--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
-
+from api.views import CategoryViewSet
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(
@@ -24,4 +24,6 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
+    path('api/v1/categories/', CategoryViewSet),
+
 ]
